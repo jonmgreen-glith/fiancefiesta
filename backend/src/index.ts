56108c223522cf5855e497b3ddeb675ff10d49bd
jonco <jonco@MUFFINDESKTOP>
2026-05-07 00:00:09 -0400

    updated endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -51,6 +51,6 @@ app.post("/token", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
